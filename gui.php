4004e0e58f80f0ea1356cb2f4207c17c6d936f96
Clayton Turner <caturner3@g.cofc.edu>
2015-04-22 15:31:05 -0400

    PHP working with wamp

--- gui.php
+++ gui.php
@@ -5,29 +5,29 @@
         <title>CHS Database</title>
         <?php 
         $servername = "localhost";
-        $username = "root":
+        $username = "root";
         $password = "admin";
         $db = "schs";
         // Open connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = mysqli_connect($servername, $username, NULL, $db);
         // Check connection
 		if ($conn->connect_error) {
 		    die("Connection failed: " . $conn->connect_error);
 		}
+		
 		?>
     </head>
     <body>
         <h1>Charleston Historical Society Database Viewer</h1>
         <p>
 	        <?php 
-	        echo "Testing";
 			$sql = "SELECT * from Constituents";
 			$result = $conn->query($sql);
 
 			if ($result->num_rows > 0) {
 		    // output data of each row
 			    while($row = $result->fetch_assoc()) {
-			        echo "Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>","Address: ", $row["address"], ", ", $row["city"],", ",$row["state"],", ",$row["zip"],"<br>","Phone: ", $row["phone"], ", ",$row["email"],"<br><br>";
+			        echo "Name: " . $row["firstName"]. " " . $row["lastName"]. "<br>","Address: ", $row["address"], ", ", $row["city"],", ",$row["state"],", ",$row["zip"],"<br>","Phone: ", $row["phone"], ", ",$row["email"],"<br><br>";
 			    }
 			} else {
 			    echo "No results for query";
