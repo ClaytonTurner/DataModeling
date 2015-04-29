4135770d98f460eadf252a1ea5bdf8005eb32113
Clayton Turner <caturner3@g.cofc.edu>
2015-04-29 13:50:36 -0400

    Updating sql injection defense

--- gui.php
+++ gui.php
@@ -26,11 +26,44 @@
 	        <?php 
 	        function injection_defense($statement){
 	        	$retval = true;
-	        	$statement = strtolower($statement);
+	        	/* lowercase and pop semi-colon */
+	        	$statement = trim(strtolower($statement),";");
 	        	$split = explode(" ",$statement);
 	        	$first = trim($split[0]);
+	        	/* Deliberate altering query */
 	        	if($first == "drop" || $first == "insert" || $first == "update" || $first == "delete")
 	        		$retval = false;
+	        	/* Tautology */
+	        	/* Doesn't work for ... OR 'x'= 'x', but
+	        	can be extrapolated if needed to take to the web */
+	        	elseif(preg_match('/=/',$statement)){
+	        		/* if we can just use spacing */
+	        		if(preg_match('/ = /',$statement)){ 
+	        			//$pos = strpos($split,'=');
+	        			$pos = array_search('=',$split);
+	        			if($split[$pos-1] == $split[$pos+1])
+	        				$retval = false;
+	        		}
+	        		/*elseif(preg_match('/= /')){
+
+	        		}
+	        		elseif(preg_match('/ =/')){
+
+	        		}*/
+	        		else{ /* our values are pushed against it */
+	        			foreach($split as &$val){
+	        				if(preg_match('/=/',$val)){
+	        					$checker = explode('=',$val);
+	        					if(count($checker) == 2){
+	        						if(trim($checker[0]) == trim($checker[1]))
+	        							$retval = false;
+	        					}
+	        					
+	        				}
+	        			}
+	        		} 
+	        	}
+	        	/* Return SQL injection ?(not) detected */
 	        	return $retval;
 	        }
 	        if(!empty($_POST["sql"])){
